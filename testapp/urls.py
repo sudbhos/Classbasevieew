913bd109ae45da5e01c0d8e263003a952f6fa4dc
sudhir <sbhosale369@gmail.com>
2022-11-13 20:25:51 +0530

    temp added

--- testapp/urls.py
+++ testapp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('<int:pk>',views.CompanyDetailView.as_view(),name="CompanyDetailView"),
     path("CompanyCreateView",views.CompanyCreateView.as_view(),name="CompanyCreateView"),
     path('update/<int:pk>',views.CampanyUpdateView.as_view(),name="CampanyUpdateView"),
+    path('delete/<int:pk>',views.CompanyDeleteViews.as_view(),name="CompanyDeleteViews"),
 
 ]
--- testapp/views.py
+++ testapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import ListView,DetailView,UpdateView,DeleteView,CreateView
 from .models import Company
 # Create your views here.
@@ -16,3 +17,8 @@ class CompanyCreateView(CreateView):
 class CampanyUpdateView(UpdateView):
     model = Company
     fields = ["name","ceo","city","ctype"]
+
+class CompanyDeleteViews(DeleteView):
+    model = Company
+    success_url=reverse_lazy("CompanyListView")
+
